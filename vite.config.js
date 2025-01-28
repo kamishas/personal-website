50223bde391830401748074e2f0eddb628bafe20
shasank kamineni <skamine3@gmu.edu>
2025-01-28 17:10:27 -0500

    Initial commit - Added portfolio website

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-website/", // 🔹 Replace with your GitHub repo name
+  server: {
+    host: true
+  }
 });
